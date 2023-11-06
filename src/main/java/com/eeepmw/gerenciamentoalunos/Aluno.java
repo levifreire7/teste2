bee9e372a6458d1984d9c51cfcb55a3d8434a2fc
levifreire7 <levifreire7@gmail.com>
2023-11-06 09:48:09 -0300

    firts commit

---
+++ src/main/java/com/eeepmw/gerenciamentoalunos/Aluno.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eeepmw.gerenciamentoalunos;
+
+/**
+ *
+ * @author levi
+ */
+public class Aluno {
+    private String matricula;
+    private String nome;
+    private String dataNascimento;
+    private String sexo;
+    private String telefone;
+    private String endereco;
+    
+    
+}
